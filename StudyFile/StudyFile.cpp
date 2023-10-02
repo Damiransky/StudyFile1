70c7fd6dc21c7c30e7c4f6d680cf897f7ff02636
Damir Zaynulin <damiransky@mail.ru>
2023-10-02 22:26:30 +0400

    First programm

--- StudyFile/StudyFile.cpp
+++ StudyFile/StudyFile.cpp
@@ -1,12 +1,24 @@
-﻿#include <iostream>
+﻿// Connect the library for Cyrillic, and to use console
+#include <iostream>
 #include <cstdlib>
+// Use namespace for new variables, and safe them
 using namespace std;
-int main() {
-	// Изменение кодировки консоли:
+// Create new function for search number of newtones
+int main(){
+	// Use Cyrillic
 	system("chcp 1251>nul");
-	// Отображение сообщения в консоли:
-	cout << "Программируем на С++!" << endl;
-	// Задержка консольного окна:
-	system("pause>nul");
-	return 0;
+	// Create constant
+	const double NEWTON_IN_TON = 9806.65;
+	// Create variables
+	double tons, newtons;
+	// Output on cmd our query 
+	cout << "Укажите количество тонн:";
+	// Waiting for response 
+	cin >> tons;
+	// Сalculate
+	newtons = tons * NEWTON_IN_TON;
+	// Give value
+	cout << "Перевод в Ньютоны составил: " << newtons << endl;
+	system("pause > nul");
+	return newtons;
 }
